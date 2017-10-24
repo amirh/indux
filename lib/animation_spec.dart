a1934dbca056956a3226853a4ad8bdc961f164cf
Amir Hardon <amirha@google.com>
2017-10-24 16:57:25 -0700

    add animation specs for all directions

--- lib/animation_spec.dart
+++ lib/animation_spec.dart
@@ -19,13 +19,19 @@ class TileMotionSpec {
 
 List<TileMotionSpec> buildMotionSpec(BoardState fromState, BoardState toState, Action action) {
     if (action?.type == ActionType.moveRight) {
-      return _buildMoveRightSpec(fromState, toState, action);
+      return _buildMoveRightSpec(fromState, toState);
+    } else if (action?.type == ActionType.moveLeft) {
+      return _buildMoveLeftSpec(fromState, toState);
+    } else if (action?.type == ActionType.moveUp) {
+      return _buildMoveUpSpec(fromState, toState);
+    } else if (action?.type == ActionType.moveDown) {
+      return _buildMoveDownSpec(fromState, toState);
     }
 
     return _buildFixedMotionSpec(toState);
 }
 
-List<TileMotionSpec> _buildMoveRightSpec(BoardState fromState, BoardState toState, Action action) {
+List<TileMotionSpec> _buildMoveRightSpec(BoardState fromState, BoardState toState) {
   int dimen = toState.dimension;
   List<TileMotionSpec> motionSpec = new List<TileMotionSpec>();
   for (int i = 0; i < dimen; i += 1) {
@@ -54,6 +60,111 @@ List<TileMotionSpec> _buildMoveRightSpec(BoardState fromState, BoardState toStat
       }
 
       prevJ -= 1;
+    }
+  }
+
+  return motionSpec;
+}
+
+List<TileMotionSpec> _buildMoveLeftSpec(BoardState fromState, BoardState toState) {
+  int dimen = toState.dimension;
+  List<TileMotionSpec> motionSpec = new List<TileMotionSpec>();
+  for (int i = 0; i < dimen; i += 1) {
+    int prevJ = 0;
+    for (int j = 0 ; j < dimen; j += 1) {
+      if(toState.tiles[i][j] == 0)
+        continue;
+
+      while (prevJ < dimen && fromState.tiles[i][prevJ] == 0) {
+        prevJ += 1;
+      }
+
+      if (i == toState.lastTileAdded?.x && j == toState.lastTileAdded?.y) {
+        continue;
+      }
+
+      motionSpec.add(new TileMotionSpec(i, prevJ, i, j));
+
+      if (toState.tiles[i][j] != fromState.tiles[i][prevJ]) {
+        prevJ += 1;
+        while (prevJ < dimen && fromState.tiles[i][prevJ] == 0) {
+          prevJ += 1;
+        }
+        motionSpec.add(new TileMotionSpec(i, prevJ, i, j));
+        motionSpec.add(new TileMotionSpec(i, j, i, j, fadeIn: true));
+      }
+
+      prevJ += 1;
+    }
+  }
+
+  return motionSpec;
+}
+
+List<TileMotionSpec> _buildMoveUpSpec(BoardState fromState, BoardState toState) {
+  int dimen = toState.dimension;
+  List<TileMotionSpec> motionSpec = new List<TileMotionSpec>();
+  for (int j = 0; j < dimen; j += 1) {
+    int prevI = 0;
+    for (int i = 0 ; i < dimen; i += 1) {
+      if(toState.tiles[i][j] == 0)
+        continue;
+
+      while (prevI < dimen && fromState.tiles[prevI][j] == 0) {
+        prevI += 1;
+      }
+
+      if (i == toState.lastTileAdded?.x && j == toState.lastTileAdded?.y) {
+        continue;
+      }
+
+      motionSpec.add(new TileMotionSpec(prevI, j, i, j));
+
+      if (toState.tiles[i][j] != fromState.tiles[prevI][j]) {
+        prevI += 1;
+        while (prevI < dimen && fromState.tiles[prevI][j] == 0) {
+          prevI += 1;
+        }
+        motionSpec.add(new TileMotionSpec(prevI, j, i, j));
+        motionSpec.add(new TileMotionSpec(i, j, i, j, fadeIn: true));
+      }
+
+      prevI += 1;
+    }
+  }
+
+  return motionSpec;
+}
+
+List<TileMotionSpec> _buildMoveDownSpec(BoardState fromState, BoardState toState) {
+  int dimen = toState.dimension;
+  List<TileMotionSpec> motionSpec = new List<TileMotionSpec>();
+  for (int j = 0; j < dimen; j += 1) {
+    int prevI = dimen - 1;
+    for (int i = dimen - 1 ; i >= 0; i -= 1) {
+      if(toState.tiles[i][j] == 0)
+        continue;
+
+      while (prevI >= 0 && fromState.tiles[prevI][j] == 0) {
+        prevI -= 1;
+      }
+
+      if (i == toState.lastTileAdded?.x && j == toState.lastTileAdded?.y) {
+        continue;
+      }
+
+      motionSpec.add(new TileMotionSpec(prevI, j, i, j));
+
+      if (toState.tiles[i][j] != fromState.tiles[prevI][j]) {
+        prevI -= 1;
+        while (prevI >= 0 && fromState.tiles[prevI][j] == 0) {
+          prevI -= 1;
+        }
+        motionSpec.add(new TileMotionSpec(prevI, j, i, j));
+        motionSpec.add(new TileMotionSpec(i, j, i, j, fadeIn: true));
+      }
+
+      prevI -= 1;
     }
   }
 
